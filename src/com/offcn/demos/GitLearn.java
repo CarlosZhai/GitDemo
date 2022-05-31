771d6fe6e72b78eed775b618916a43f1a51428bc
Alan <zhaijia421182@163.com>
2022-05-31 12:19:17 +0800

    clone远程仓库到本地

--- src/com/offcn/demos/GitLearn.java
+++ src/com/offcn/demos/GitLearn.java
@@ -3,5 +3,6 @@ package com.offcn.demos;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("Git Learn!");
+        System.out.println("clone operation");
     }
 }
